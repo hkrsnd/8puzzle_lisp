29d75677b45b355006349652dc4555a7ba9684e4
pii5656 <microapple56@gmail.com>
2016-04-25 01:29:15 +0900

    man-dis

--- main.lisp
+++ main.lisp
@@ -1,24 +1,24 @@
 ;; initialize pazzle
 (defun init ()
-  ;; initial pazzle list
+  ;; initial puzzle list
   (defun init-loop (i)
     (if (> i 1)
 	(cons 0  (init-loop (- i 1)))
 	(list 0)))
   ;; set initial numbers
-  (defun set-numbers (ls pazzle)
+  (defun set-numbers (ls puzzle)
     (let ((a  (random 9)))
       (if (not (equal  (length ls) 0))
-	  (if (equal (nth a pazzle) 0)
+	  (if (equal (nth a puzzle) 0)
 	      (progn
-		(setf (nth a pazzle) (first ls))
-		(set-numbers (rest ls) pazzle))
-	      (set-numbers ls pazzle))
-	  pazzle)))
+		(setf (nth a puzzle) (first ls))
+		(set-numbers (rest ls) puzzle))
+	      (set-numbers ls puzzle))
+	  puzzle)))
   (set-numbers (list 1 2 3 4 5 6 7 8 ) (init-loop 9)))
 
 ;; print puzzle as a table
-(defun print-pazzle (ls)
+(defun print-puzzle (ls)
   (pprint (get-list-from-head ls 3))
   (pprint (get-list-from-head (cdddr ls) 3))
   (pprint (get-list-from-head (cdddr (cdddr ls)) 3)))
@@ -29,31 +29,45 @@
       (cons (car ls) (get-list-from-head (cdr ls) (- n 1)))
       '()))
 
-(defun find-zero-pos (pazzle pos)
-  (if (equal (car pazzle) 0)
+(defun find-zero-pos (puzzle pos)
+  (if (equal (car puzzle) 0)
       pos
-      (find-zero-pos (cdr pazzle) (+ pos 1))))
+      (find-zero-pos (cdr puzzle) (+ pos 1))))
 
-(defun move-panel (pazzle direct)
-  (let ((zero (find-zero-pos pazzle 0)))
+(defun move-panel (puzzle direct)
+  (let ((zero (find-zero-pos puzzle 0)))
     (cond
       ((equal direct 'u)
        (if (and (>= zero 6) (<= zero 8))
 	   (error "**invalid input**")
-	   (rotatef (nth zero pazzle) (nth (+ zero 3) pazzle))))
+	   (rotatef (nth zero puzzle) (nth (+ zero 3) puzzle))))
       ((equal direct 'd)
        (if (and (>= zero 0) (<= zero 2))
 	   (error "**invalid input**")
-	   (rotatef (nth zero pazzle) (nth (- zero 3) pazzle))))
+	   (rotatef (nth zero puzzle) (nth (- zero 3) puzzle))))
       ((equal direct 'l)
        (if (or (equal zero 2) (equal zero 5) (equal zero 8))
 	   (error "**invalid input**")
-	   (rotatef (nth zero pazzle) (nth (+ zero 1) pazzle))))
+	   (rotatef (nth zero puzzle) (nth (+ zero 1) puzzle))))
       ((equal direct 'r)
        (if (or (equal zero 0) (equal zero 3) (equal zero 6))
 	   (error "**invalid input**"))
-       	   (rotatef (nth zero pazzle) (nth (- zero 1) pazzle))))
+       	   (rotatef (nth zero puzzle) (nth (- zero 1) puzzle))))
 
-;;  (rotatef (nth panel-pos pazzle) (nth zero pazzle))
-  (print-pazzle pazzle)
-  pazzle))
+;;  (rotatef (nth panel-pos puzzle) (nth zero puzzle))
+  (print-puzzle puzzle)
+  puzzle))
+
+;; calculate numbers of panels on different place
+(defun calc-diff-panel-num (puzzle)
+  (defun calc-diff-panel-num-loop (puzzle num-list diff-num)
+    (if (equal puzzle '())
+	diff-num
+	(if (and (not (equal (car puzzle) (car num-list))) (not (equal (car puzzle) 0)))
+	    (calc-diff-panel-num-loop (cdr puzzle) (cdr num-list) (1+ diff-num))
+	    (calc-diff-panel-num-loop (cdr puzzle) (cdr num-list) diff-num))))
+  (calc-diff-panel-num-loop puzzle '(1 2 3 4 5 6 7 8) 0))
+
+;; calculate the sum of manhattan distance
+(defun calc-manhat-dist (puzzle)
+  )
